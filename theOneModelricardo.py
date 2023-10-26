ed4d485b705d4c4532f2c1264ff0657ee57b152e
pascal.raos <pascal.raos@coinmetrics.io>
2023-10-26 13:51:26 +0100

    complex model

--- theOneModelricardo.py
+++ theOneModelricardo.py
@@ -19,7 +19,7 @@ from keras.layers import Conv2D, MaxPooling2D, LeakyReLU
 
 
 import pandas as pd
-import keras.utils as to_categorical
+from keras.utils import to_categorical
 
 reverse_dict = {
     'hash': '#',
@@ -167,26 +167,29 @@ def createModel(trainX, shape, num_classes):
     return model
 
 
-def create_model(captcha_length, captcha_num_symbols, input_shape, model_depth=5, module_size=2):
-  input_tensor = keras.Input(input_shape)
-  x = input_tensor
-  for i, module_length in enumerate([module_size] * model_depth):
-      print(f'module length is {module_length}, module size is {[module_size] * model_depth}, i = {i}')
-      for j in range(module_length):
-          x = keras.layers.Conv2D(32*2**min(i, 3), kernel_size=3, padding='same', kernel_initializer='he_uniform')(x)
-          x = keras.layers.BatchNormalization()(x)
-          x = keras.layers.Activation('relu')(x)
-      x = keras.layers.MaxPooling2D(2)(x)
+def createModelComplex(trainX, shape, num_classes):
 
-  x = keras.layers.Flatten()(x)
-  x = [keras.layers.Dense(captcha_num_symbols, activation='softmax', name='char_%d'%(i+1))(x) for i in range(captcha_length)]
-  model = keras.Model(inputs=input_tensor, outputs=x)
-
-  model.compile(loss='categorical_crossentropy',
-                      optimizer=keras.optimizers.Adam(1e-3, amsgrad=True),
-                      metrics=['accuracy'])
-  model.summary()
-  return model
+    model = keras.Sequential()
+    
+    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=shape))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(0.5))
+    model.add(Flatten())
+    
+    model.add(Dense(128, activation='relu', kernel_regularizer=regularizers.l2(0.001)))
+    model.add(Dense(num_classes, activation='softmax', kernel_regularizer=regularizers.l2(0.001)))
+    
+    model.compile(loss="categorical_crossentropy", optimizer='adam', metrics=["accuracy"])
+    model.summary()
+    
+    return model
 
 def splitY(trainY, num_images, captcha_len):
 
@@ -205,11 +208,11 @@ def splitY(trainY, num_images, captcha_len):
 
     return temp
 
-def smallerCaptchaArray():
+def smallerCaptchaArray(index):
     X = []
     y = []
 
-    for features, label in train[0]:
+    for features, label in train[index]:
             X.append(features)
             y.append(label)
 
@@ -228,6 +231,15 @@ def smallerCaptchaArray():
 
     print(Y.shape)
     print(X.shape)
+
+    X.reshape(-1, 64, 128, 1)
+    Y = to_categorical(y)
+
+    X=X.astype('float32')
+
+    shape = (len(X[0]), len(X[0][0]), 1)
+
+    return X, Y, shape
 
 def hugeCaptchaArray():
 
@@ -267,7 +279,7 @@ def hugeCaptchaArray():
 
 if __name__ == '__main__':
    
-    directory = 'test1'
+    directory = 'trainvalues'
 
     with open('symbols.txt') as symbols_file:
         captcha_symbols = symbols_file.readline()
@@ -298,22 +310,16 @@ if __name__ == '__main__':
 
     #list of list implementation
 
-    smallerCaptchaArray()
-
-    # print(shape)
-
-    # X=X.astype('float32')
-
-    # shape = (len(X[0][0]), len(X[0][0][0]), 1)
+    X1,Y1,shape = smallerCaptchaArray(1)
     # # #print(f'The input image is {trainX[0]} and the output associated is {trainYs[0][0]}')
 
-    # first_model = createModel(X[0], shape, 45)
+    first_model = createModelComplex(X1, shape, 45)
     # print(Y[0].shape)
     # print(X[0].shape)
     # # second_model = create_model(5, 43, shape)
-    # batch_size = 64
+    batch_size = 64
 
 
-    # first_model.fit(X[0], Y[0], epochs=20, batch_size=batch_size, validation_split=0.2)
+    first_model.fit(X1, Y1, epochs=20, batch_size=batch_size, validation_split=0.2, callbacks=[keras.callbacks.ModelCheckpoint("best model at epoch:{epoch}.h5", save_best_only=True)])
 
-    # first_model.save('please_work.h5')
+    first_model.save('character2complex.h5')
